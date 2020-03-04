880dae7669a90f51c8bfe2b438d87dd7ff2728c7
Dan Suh <kaehops@gmail.com>
2020-03-04 18:43:48 +0900

    comments

--- causal_meta/models/binary.py
+++ causal_meta/models/binary.py
@@ -5,40 +5,49 @@ from itertools import chain
 
 from causal_meta.utils.torch_utils import logsumexp
 
+
 class BinaryStructuralModel(nn.Module):
     def __init__(self, model_A_B, model_B_A):
         super(BinaryStructuralModel, self).__init__()
         self.model_A_B = model_A_B
         self.model_B_A = model_B_A
-        self.w = nn.Parameter(torch.tensor(0., dtype=torch.float64))
+        self.z = nn.Parameter(torch.tensor(0., dtype=torch.float64))
 
     def forward(self, inputs):
         return self.online_loglikelihood(self.model_A_B(inputs), self.model_B_A(inputs))
 
     def online_loglikelihood(self, logl_A_B, logl_B_A):
         n = logl_A_B.size(0)
-        log_alpha, log_1_m_alpha = F.logsigmoid(self.w), F.logsigmoid(-self.w)
+        log_alpha, log_1_m_alpha = F.logsigmoid(self.z), F.logsigmoid(-self.z)
 
-        return logsumexp(log_alpha + torch.sum(logl_A_B),
-            log_1_m_alpha + torch.sum(logl_B_A))# / float(n)
+        return logsumexp(
+            log_alpha + torch.sum(logl_A_B),
+            log_1_m_alpha + torch.sum(logl_B_A))
 
     def modules_parameters(self):
+        # parameters for conditional models
         return chain(self.model_A_B.parameters(), self.model_B_A.parameters())
 
     def structural_parameters(self):
-        return [self.w]
+        # meta-parameter "z" that determines which conditional model is more probable
+        return [self.z]
+
 
 class ModelA2B(nn.Module):
+    # models A -> B
     def __init__(self, marginal, conditional):
         super(ModelA2B, self).__init__()
         self.p_A = marginal
         self.p_B_A = conditional
 
     def forward(self, inputs):
-        inputs_A, inputs_B = torch.split(inputs, 1, dim=1)
+        inputs_A, inputs_B = torch.split(inputs, split_size_or_sections=1, dim=1)
+        # sum of log-softmax values
         return self.p_A(inputs_A) + self.p_B_A(inputs_B, inputs_A)
 
+
 class ModelB2A(nn.Module):
+    # models B -> A
     def __init__(self, marginal, conditional):
         super(ModelB2A, self).__init__()
         self.p_B = marginal
@@ -46,4 +55,5 @@ class ModelB2A(nn.Module):
 
     def forward(self, inputs):
         inputs_A, inputs_B = torch.split(inputs, 1, dim=1)
+        # sum of log-softmax values
         return self.p_B(inputs_B) + self.p_A_B(inputs_A, inputs_B)
--- causal_meta/modules/categorical.py
+++ causal_meta/modules/categorical.py
@@ -1,25 +1,50 @@
 import torch
 import torch.nn as nn
 
+
 class Marginal(nn.Module):
     def __init__(self, N, dtype=None):
-        super(Marginal, self).__init__()
+        super().__init__()
         self.N = N
+        # marginal probabilities; w[i] = P(A = i)
         self.w = nn.Parameter(torch.zeros(N, dtype=dtype))
-    
+
+    def __str__(self):
+        return f'N = {self.N}\nw = {self.w}'
+
     def forward(self, inputs):
-        # log p(A) / log p(B)
-        cste = torch.logsumexp(self.w, dim=0)
-        return self.w[inputs.squeeze(1)] - cste
+        # log p(A) or log p(B)
+        return self.logsoftmax(inputs)
+
+    def logsoftmax(self, inputs):
+        denom = torch.logsumexp(self.w, dim=0)
+        # log(exp(p(input)) / sum_i(exp(p(x_i)))) = log-softmax
+        return self.w[inputs.squeeze(dim=1)] - denom
+
 
 class Conditional(nn.Module):
     def __init__(self, N, dtype=None):
-        super(Conditional, self).__init__()
+        super().__init__()
         self.N = N
+        # conditional probabilities; w[i][j] = P(B = j | A = i)
         self.w = nn.Parameter(torch.zeros((N, N), dtype=dtype))
-    
+
+    def __str__(self):
+        return f'N = {self.N}\nw = {self.w}'
+
     def forward(self, inputs, conds):
-        # log p(B | A) / log p(A | B)
-        conds_ = conds.squeeze(1)
-        cste = torch.logsumexp(self.w[conds_], dim=1)
-        return self.w[conds_, inputs.squeeze(1)] - cste
+        return self.conditional_logsoftmax(inputs, conditional_values=conds)
+
+    def conditional_logsoftmax(self, inputs, conditional_values):
+        # log p(B | A) or log p(A | B)
+        conds_ = conditional_values.squeeze(1)
+        denoms = torch.logsumexp(self.w[conds_], dim=1)
+        # log-softmax
+        return self.w[conds_, inputs.squeeze(1)] - denoms
+
+
+if __name__ == '__main__':
+    m = Marginal(10)
+    dummy_input = torch.LongTensor(10, 1).random_(10)
+    print(m)
+    print(m(dummy_input))
