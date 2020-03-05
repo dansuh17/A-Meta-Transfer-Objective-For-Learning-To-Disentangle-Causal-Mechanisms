793ae7cf3b9e60303cd79c0abe7eade666ab1c37
Dan Suh <kaehops@gmail.com>
2020-03-06 02:13:30 +0900

    update notebooks

--- notebooks/bivariate-categorical/models.py
+++ notebooks/bivariate-categorical/models.py
@@ -6,6 +6,7 @@ import torch.nn as nn
 from causal_meta.modules.categorical import Marginal, Conditional
 from causal_meta.models.binary import BinaryStructuralModel, ModelA2B, ModelB2A
 
+
 class Model(object):
     def __init__(self, N):
         self.N = N
@@ -31,17 +32,22 @@ class Model(object):
 
         return self.set_ground_truth(pi_A, pi_B_A)
 
+
 class Model1(Model, ModelA2B):
     def __init__(self, N, dtype=None):
         Model.__init__(self, N)
         ModelA2B.__init__(self, Marginal(N, dtype=dtype), Conditional(N, dtype=dtype))
 
     def set_ground_truth(self, pi_A, pi_B_A):
+        # set parameters to ground truth values
         pi_A_th = torch.from_numpy(pi_A)
         pi_B_A_th = torch.from_numpy(pi_B_A)
-        
+
+        # P(A)
         self.p_A.w.data = torch.log(pi_A_th)
+        # P(B | A)
         self.p_B_A.w.data = torch.log(pi_B_A_th)
+
 
 class Model2(Model, ModelB2A):
     def __init__(self, N, dtype=None):
@@ -49,23 +55,27 @@ class Model2(Model, ModelB2A):
         ModelB2A.__init__(self, Marginal(N, dtype=dtype), Conditional(N, dtype=dtype))
 
     def set_ground_truth(self, pi_A, pi_B_A):
+        # set parameters to ground truth values
         pi_A_th = torch.from_numpy(pi_A)
         pi_B_A_th = torch.from_numpy(pi_B_A)
-        
+
+        # joint probability P(A, B) = P(A)P(B | A)
         log_joint = torch.log(pi_A_th.unsqueeze(1)) + torch.log(pi_B_A_th)
-        log_p_B = torch.logsumexp(log_joint, dim=0)
-        
-        self.p_B.w.data = log_p_B
-        self.p_A_B.w.data = log_joint.t() - log_p_B.unsqueeze(1)
+        log_p_B = torch.logsumexp(log_joint, dim=0)  # get P(B)
+
+        self.p_B.w.data = log_p_B  # P(B)
+        self.p_A_B.w.data = log_joint.t() - log_p_B.unsqueeze(1)  # P(A | B)
+
 
 class StructuralModel(BinaryStructuralModel):
     def __init__(self, N, dtype=None):
-        model_A_B = Model1(N, dtype=dtype)
-        model_B_A = Model2(N, dtype=dtype)
+        model_A_B = Model1(N, dtype=dtype)  # conditional model A -> B
+        model_B_A = Model2(N, dtype=dtype)  # conditional model B -> A
         super(StructuralModel, self).__init__(model_A_B, model_B_A)
-        self.w = nn.Parameter(torch.tensor(0., dtype=dtype))
+        self.z = nn.Parameter(torch.tensor(0., dtype=dtype))
     
     def set_ground_truth(self, pi_A, pi_B_A):
+        # set parameters to ground truth values
         self.model_A_B.set_ground_truth(pi_A, pi_B_A)
         self.model_B_A.set_ground_truth(pi_A, pi_B_A)
     
